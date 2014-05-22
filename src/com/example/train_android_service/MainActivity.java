28357ad5497c60958229723bb181895deb7db8b8
Thai <zzmrthaizz@gmail.com>
2014-05-22 19:52:50 +0700

    service send notification

--- src/com/example/train_android_service/MainActivity.java
+++ src/com/example/train_android_service/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.train_android_service;
 
 import android.os.Bundle;
+import android.R.id;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	/** Called when the activity is first created. */
+	 @Override
+	 public void onCreate(Bundle savedInstanceState) {
+	     super.onCreate(savedInstanceState);
+	     setContentView(R.layout.activity_main);
+	     Button buttonStartService = (Button)findViewById(R.id.btn_start);
+	  
+	     buttonStartService.setOnClickListener(new Button.OnClickListener(){
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+	 @Override
+	 public void onClick(View arg0) {
+	  // TODO Auto-generated method stub
+	  Intent intent = new Intent(MainActivity.this, MyService.class);
+	  startService(intent);
+	 }});
+	  
+	  
+	 }
 }
